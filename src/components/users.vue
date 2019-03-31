8a9658350277b6a2de957de573448cfa13ee1c8e
chq <1539917854@qq.com>
2019-03-31 20:54:35 +0800

    完成新增用户

--- src/components/users.vue
+++ src/components/users.vue
@@ -21,7 +21,7 @@
         </el-input>
       </el-col>
       <el-col :span="4">
-        <el-button class="my-btn" type="success" @click="addUser" plain>添加用户</el-button>
+        <el-button class="my-btn" type="success" @click="addFormVisible = true" plain>添加用户</el-button>
       </el-col>
     </el-row>
     <!-- 表格  使用自定义列模板 -->
@@ -66,6 +66,28 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+
+    <!-- 新增用户表单  要进行表单验证 -->
+    <el-dialog title="添加用户" :visible.sync="addFormVisible">
+      <el-form :model="addForm" :rules="addRules" ref="addForm">
+        <el-form-item label="用户名" prop="username" label-width="100px">
+          <el-input v-model="addForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password" label-width="100px">
+          <el-input v-model="addForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="addForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="addForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitAdd('addForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -82,7 +104,37 @@ export default {
         pagesize: 10
       },
       // 用户数组
-      userList: []
+      userList: [],
+      // 是否显示新增用户表单
+      addFormVisible: false,
+      // 添加用户表单数据
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      // 新增表单验证
+      addRules: {
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+          {
+            min: 3,
+            max: 16,
+            message: "用户名长度在 3 到 16 个字符",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 16,
+            message: "密码长度在 6 到 16 个字符",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -120,6 +172,25 @@ export default {
       // 接口调用  请求路径：users/:uId/state/:type
       // console.log(row);
       this.$axios.put(`users/${row.id}/state/${row.mg_state}`);
+    },
+    // 新增用户
+    submitAdd(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          // 进入这里表示能够新增
+          // 调用接口  然后新增成功关闭新增表单的对话框
+          let res = await this.$axios.post("users", this.addForm);
+          console.log(res);
+          if (res.data.meta.status === 201) {
+            // 新增成功  调用查询方法
+            this.queryUserList();
+          }
+          this.addFormVisible = false;
+        } else {
+          this.$message.error("亲,请正确填写信息哦");
+          return false;
+        }
+      });
     }
   },
   // 使用钩子函数在页面加载之前获取数据
